2c1f921c1976c903c435250958c01598f8482259
rohancodex <77398907+rohancodex@users.noreply.github.com>
2022-12-30 11:05:31 +0530

    updated script

--- slacker.py
+++ slacker.py
@@ -1,13 +1,35 @@
+import time, pyautogui
+import PySimpleGUI as sg
+import multiprocessing
 
-import pyautogui
-from time import sleep
 
-num=1
-while(True):
-    if(num%2==0):
-        pyautogui.moveRel(100, 0, duration = 1)
-    else:
-        pyautogui.moveRel(-100, 0, duration = 1)
-    num+=1
-    # Adjust the sleep time as you wish
-    sleep(60);
+def KeepUI():
+    
+    sg.theme('Dark')
+    layout = [
+        [sg.Text('Happy Slacking!\nYou can keep this window minimised, and it will continue running.\nClose it to disable it.')]
+    ]
+    window = sg.Window('Slacker', layout)
+    
+    p2 = multiprocessing.Process(target = dontsleep)
+    p2.start()
+    
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED: # if user closes window or clicks cancel
+            if p2.is_alive(): 
+                p2.terminate()
+            break
+
+def dontsleep():
+    while True:
+        pyautogui.press('volumedown')
+        time.sleep(1)
+        pyautogui.press('volumeup')
+        time.sleep(300)
+
+
+
+if __name__ == '__main__':
+    p1 = multiprocessing.Process(target = KeepUI)
+    p1.start()
